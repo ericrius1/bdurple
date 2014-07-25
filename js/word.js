f3832b7dd44445af7ee535bf849e2ac5de2c059c
Eric <Ericrius1@gmail.com>
2014-07-25 17:58:03 +0200

    transition;

--- js/word.js
+++ js/word.js
@@ -3,39 +3,38 @@ var Text = function() {
   var curEmitterIndex = 0;
   var emitters  = [];
 
-  var wordGeo = new THREE.TextGeometry('Peace', {
-    size: 10,
+  var shalomGeo = new THREE.TextGeometry('Shalom', {
+    size: 4,
     height: 1,
-    curveSegments: 8,
+    curveSegments: 6,
     font: 'josefin slab'
   });
 
   var anchor = new THREE.Object3D();
-  var word = new THREE.Mesh(wordGeo);
-  word.position.x -=10;
+  var shalomWord = new THREE.Mesh(shalomGeo);
+  shalomWord.position.x -=8;
   scene.add(anchor)
-  anchor.add(word)
-  word.visible = false;
+  anchor.add(shalomWord)
+  shalomWord.visible = false;
 
-  var wordPoints = THREE.GeometryUtils.randomPointsInGeometry(wordGeo, 500);
+  var wordPoints = THREE.GeometryUtils.randomPointsInGeometry(shalomGeo, 1000);
 
   var particleGroup = new SPE.Group({
     texture: THREE.ImageUtils.loadTexture('assets/star.png'),
-    maxAge: 4
+    maxAge: 2
   });
+  createEmitterPoints();
+  shalomWord.add(particleGroup.mesh);
+  findTarget();
+  
+
 
   function getEmitterParams(){
     return {
       position: new THREE.Vector3(0, 30, 0),
-      accelerationSpread: new THREE.Vector3(2, 2, 2)
+      accelerationSpread: new THREE.Vector3(.1, .1, .1)
     }
   }
-
-  createEmitterPoints();
-  
-  word.add(particleGroup.mesh);
-  findTarget();
-
   function createEmitterPoints(){
     for(var i = 0; i < wordPoints.length; i++){
       var emitter = new SPE.Emitter(getEmitterParams());
@@ -62,17 +61,16 @@ var Text = function() {
     }
 
     var moveTween = new TWEEN.Tween(curPos).
-      to(targetPos, 100).
+      to(targetPos, 10000).
       onUpdate(function(){
         emitter.position.set(curPos.x, curPos.y, curPos.z);
       }).start();
-    moveTween.onComplete(function(){
-      if(curEmitterIndex++ < emitters.length){
-        findTarget();
-      }
-    })
 
-
+    if(++curEmitterIndex < emitters.length){
+      setTimeout(function(){
+        findTarget()
+      }, 10)
+    }
   }
 
   this.update = function(){
